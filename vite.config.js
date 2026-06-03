516b085bf67b1c6f4e9ac9a18f866179bff3ee48
Wade052 <drewade057@gmail.com>
2026-06-03 13:54:06 -0400

    Update base path in Vite configuration for lottery application

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'src',
-  base: '/uikit/',
+  base: '/LotteryApp/',
   build: {
     outDir: '../dist',
     emptyOutDir: true
